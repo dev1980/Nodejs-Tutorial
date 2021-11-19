a3b1777c6e1a32e2495fb0a105f21238b303dcda
Brham Dev Mahato <brham1980@gmail.com>
2021-11-19 22:12:00 +0545

    dir function

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 12
+
+if (amount < 10) {
+    console.log("small number");
+} else {
+    console.log("its large number")
+}
+
+console.log(`hey hello world`)
--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-// const amount = 12
-
-// if (amount < 10) {
-//     console.log("small number");
-// } else {
-//     console.log("its large number")
-// }
-
-// console.log(`hey hello world`)
-
-
-
 // GLOBAL - NO WINDOW!!!
 // __diname - path to current directory
 // __filename - file name
 //require - function to use modules (CommonJS)
 //module -info about current module(file)
 //process -info about env where the program is being executed
+
+console.log(__dirname)
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
