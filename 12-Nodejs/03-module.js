10c70fd1707c061700370b782419904f719d3772
Brham Dev Mahato <brham1980@gmail.com>
2021-11-25 21:51:02 +0545

    function execution

--- 12-Nodejs/03-module.js
+++ 12-Nodejs/03-module.js
@@ -2,5 +2,6 @@
 // we can import using require functions.
 
 const names = require('./04-name');
+require('./07-mind-grinade')
 
 console.log(names)
---
+++ 12-Nodejs/07-mind-grinade.js
@@ -0,0 +1,7 @@
+let a = 5;
+let b = 6
+
+function add(){
+console.log(`the sum of a and b is ${a + b}`)
+}
+add()
